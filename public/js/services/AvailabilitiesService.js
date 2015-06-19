58e6f331e6fe044cc6047c8eb33961524b0a6aa5
rails-dev <rails-dev@GR-54.(none)>
2015-06-19 16:07:15 +0530

    add availability to cleaner

--- public/js/services/AvailabilitiesService.js
+++ public/js/services/AvailabilitiesService.js
@@ -5,11 +5,19 @@ app.factory('AvailabilitiesService', function($firebaseAuth, FIREBASE_URL, $fire
     var firebaseAvailabilities = $firebase(ref.child('availabilities'));
 
     var cleanerAvailabilities = {
+        getCAById : function (availabilityID) {
+            return $firebase(ref.child('availabilities').child(availabilityID));
+        },
     	getCleanerAvailabilities : function (cleanerID) {
             return $firebase(ref.child('availabilities').orderByChild("cleaner_id").equalTo(cleanerID)).$asArray();
         },
     	addCleanerAvailabilities: function (availabilities) {
+            console.log(availabilities);
             return firebaseAvailabilities.$push(availabilities);
+        },
+        updateCleanerAvailabilities : function(id, availability){
+           var cp = this.getCAById(id);
+           return cp.$update(availability);
         }
     }
     return cleanerAvailabilities;
