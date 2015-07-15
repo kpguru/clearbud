e2bdf7da02b36b1b8047caacdb34aab3931fe461
Pravin <ppal@grepruby.com>
2015-07-15 14:23:12 +0530

    resolve tab error on home page

--- public/js/controllers/CleanerController.js
+++ public/js/controllers/CleanerController.js
@@ -1,7 +1,6 @@
 'use strict';
 
 	app.controller('CleanerController', function ($routeParams, BookingService, CustomerService, RatingService, ServicesService, ChargesService, AvailabilitiesService, $http, $scope, $window, $location,AuthenticationService, CleanerService, $firebase, toaster, FIREBASE_URL) { 
-    $scope.signedIn = AuthenticationService.signedIn;
     $scope.profile ={};
     var currentuser={};
     $scope.about ={};
@@ -16,92 +15,92 @@
     $scope.selection1 = $scope.steps1[0];        
     var ref = new Firebase(FIREBASE_URL);
     $scope.bankinfo ={};
+    
+    if($routeParams.cleanerID){ 
+      var clanerProfile = AuthenticationService.getCurrentUser($routeParams.cleanerID);
+      clanerProfile.$loaded().then(function (clanerProfile) { 
+        $scope.clanerProfile = clanerProfile;
+        if(angular.isUndefined($scope.clanerProfile.cleaner_about_us))                    
+          $scope.save =true;
+        else
+          $scope.update =true;
+      });    
+      
+      var clanerAvailabilities = AvailabilitiesService.getCleanerAvailabilities($routeParams.cleanerID);
+      clanerAvailabilities.$loaded().then(function (data) {
+        if(clanerAvailabilities[0]){                   
+          $scope.Availabilities = clanerAvailabilities[0]; 
+          $scope.sunday = clanerAvailabilities[0].sunday;
+          $scope.monday = clanerAvailabilities[0].monday;
+          $scope.tuesday = clanerAvailabilities[0].tuesday;
+          $scope.wednesday = clanerAvailabilities[0].wednesday;
+          $scope.thusday = clanerAvailabilities[0].thusday;
+          $scope.friday = clanerAvailabilities[0].friday;
+          $scope.saturday = clanerAvailabilities[0].saturday;
+          $scope.everyday = clanerAvailabilities[0].everyday;
+          $scope.weekends = clanerAvailabilities[0].weekends;                  
+          $scope.montofri = clanerAvailabilities[0].monday_to_friday;
+        }
+      }); 
+        
+      var clanerCharges = ChargesService.getCharges($routeParams.cleanerID);
+      clanerCharges.$loaded().then(function (data) { 
+        $scope.charges = data;
+        angular.forEach( $scope.charges, function(value){
+          $scope.chrg ={   
+                          every_four_weeks  : value.every_four_weeks,
+                          every_two_weeks   : value.every_two_weeks,
+                          one_time          : value.one_time,
+                          weekly            : value.weekly,
+                          $id               : value.$id,
+                          cleaner_id        :value.cleaner_id
+                       }
+        })
+      });
+         
+      var clanerRating = RatingService.getCleanerRatings($routeParams.cleanerID);
+      clanerRating.$loaded().then(function (data) {
+        if(data.length > 0){
+          var c = 0;
+          var flag = 0
+          var sum = 0;
+          angular.forEach(data, function(value1){
+            if(value1.rating){
+              $scope.manageReviews = false;
+              angular.forEach(value1.rating, function(value){
+                sum = sum + parseInt(value.average_rating); 
+                var customerInfo = CustomerService.getCustomer(value.customer_id);                          
+                customerInfo.$asObject().$loaded().then(function (data) {
+                  $scope.customerReviews.push({customer_review: value.customer_review, customer_image: data.customer_image, firstname: data.firstname, lastname: data.lastname, date: value.date, customer_average_rating: value.average_rating});
+                });
+                c++;
+                flag++;
+              })
+              $scope.average_rating = Math.round(sum / c);
+            }
+          });
+          if(flag == 0){
+            $scope.average_rating = 0;
+          }
+        }else{
+           $scope.average_rating = 0;
+          }
+      });                  
+   
+      var cleanerService = ServicesService.getServices($routeParams.cleanerID);
+      cleanerService.$loaded().then(function (data) { 
+        $scope.services = data;
+        $scope.serviceID = $scope.services[0].$id;
+        $scope.cleanerID = $scope.services[0].cleaner_id;
+        $scope.generalServices = $scope.services[0].general[0];
+        $scope.bathroomServices = $scope.services[0].bathroom[0];
+        $scope.bedroomServices = $scope.services[0].bedroom[0];
+        $scope.livingAreaServices = $scope.services[0].livingArea[0];  
+      });  
+    }
+    
     ref.onAuth(function(authUser) {
       if (authUser != null) {
-        if($routeParams.cleanerID){ 
-
-          var clanerProfile = AuthenticationService.getCurrentUser($routeParams.cleanerID);
-          clanerProfile.$loaded().then(function (clanerProfile) { 
-            $scope.clanerProfile = clanerProfile;
-            if(angular.isUndefined($scope.clanerProfile.cleaner_about_us))                    
-              $scope.save =true;
-            else
-              $scope.update =true;
-          });    
-          
-          var clanerAvailabilities = AvailabilitiesService.getCleanerAvailabilities($routeParams.cleanerID);
-          clanerAvailabilities.$loaded().then(function (data) {
-            if(clanerAvailabilities[0]){                   
-              $scope.Availabilities = clanerAvailabilities[0]; 
-              $scope.sunday = clanerAvailabilities[0].sunday;
-              $scope.monday = clanerAvailabilities[0].monday;
-              $scope.tuesday = clanerAvailabilities[0].tuesday;
-              $scope.wednesday = clanerAvailabilities[0].wednesday;
-              $scope.thusday = clanerAvailabilities[0].thusday;
-              $scope.friday = clanerAvailabilities[0].friday;
-              $scope.saturday = clanerAvailabilities[0].saturday;
-              $scope.everyday = clanerAvailabilities[0].everyday;
-              $scope.weekends = clanerAvailabilities[0].weekends;                  
-              $scope.montofri = clanerAvailabilities[0].monday_to_friday;
-            }
-          }); 
-            
-          var clanerCharges = ChargesService.getCharges($routeParams.cleanerID);
-          clanerCharges.$loaded().then(function (data) { 
-            $scope.charges = data;
-            angular.forEach( $scope.charges, function(value){
-              $scope.chrg ={   
-                              every_four_weeks  : value.every_four_weeks,
-                              every_two_weeks   : value.every_two_weeks,
-                              one_time          : value.one_time,
-                              weekly            : value.weekly,
-                              $id               : value.$id,
-                              cleaner_id        :value.cleaner_id
-                           }
-            })
-          });
-             
-          var clanerRating = RatingService.getCleanerRatings($routeParams.cleanerID);
-          clanerRating.$loaded().then(function (data) {
-            if(data.length > 0){
-              var c = 0;
-              var flag = 0
-              var sum = 0;
-              angular.forEach(data, function(value1){
-                if(value1.rating){
-                  $scope.manageReviews = false;
-                  angular.forEach(value1.rating, function(value){
-                    sum = sum + parseInt(value.average_rating); 
-                    var customerInfo = CustomerService.getCustomer(value.customer_id);                          
-                    customerInfo.$asObject().$loaded().then(function (data) {
-                      $scope.customerReviews.push({customer_review: value.customer_review, customer_image: data.customer_image, firstname: data.firstname, lastname: data.lastname, date: value.date, customer_average_rating: value.average_rating});
-                    });
-                    c++;
-                    flag++;
-                  })
-                  $scope.average_rating = Math.round(sum / c);
-                }
-              });
-              if(flag == 0){
-                $scope.average_rating = 0;
-              }
-            }else{
-               $scope.average_rating = 0;
-              }
-          });                  
-       
-          var cleanerService = ServicesService.getServices($routeParams.cleanerID);
-          cleanerService.$loaded().then(function (data) { 
-            $scope.services = data;
-            $scope.serviceID = $scope.services[0].$id;
-            $scope.cleanerID = $scope.services[0].cleaner_id;
-            $scope.generalServices = $scope.services[0].general[0];
-            $scope.bathroomServices = $scope.services[0].bathroom[0];
-            $scope.bedroomServices = $scope.services[0].bedroom[0];
-            $scope.livingAreaServices = $scope.services[0].livingArea[0];  
-          });  
-        }
-                        
         var users = AuthenticationService.getCurrentUser(authUser.uid);
         users.$loaded().then(function (currentuser) { 
           $scope.currentUser = currentuser;
@@ -160,16 +159,6 @@
           });
         }
          
-        $scope.getCurrentStepIndex1 = function(){
-          return _.indexOf($scope.steps1, $scope.selection1);
-        };
-        
-        $scope.goToStep1 = function(index) {
-          if ( !_.isUndefined($scope.steps1[index]) ){
-            $scope.selection1 = $scope.steps1[index];
-          }
-        };
-        
         $scope.selection = $scope.steps[0];
         $scope.getCurrentStepIndex = function(){
           return _.indexOf($scope.steps, $scope.selection);
@@ -252,7 +241,16 @@
         }
              
         }
-    });    
+    }); 
+    $scope.getCurrentStepIndex1 = function(){
+      return _.indexOf($scope.steps1, $scope.selection1);
+    };
+        
+    $scope.goToStep1 = function(index) {
+      if ( !_.isUndefined($scope.steps1[index]) ){
+        $scope.selection1 = $scope.steps1[index];
+      }
+    };   
     function get_states(modelName, selected) {
       return $http.get('js/data/states.json').then(function (res) {
           modelName.state = '';
